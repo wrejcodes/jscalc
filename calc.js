5f8e1024d0238d2bd016a5d66096f2c5d744efd8
Will Jackson <wrejcodes@gmail.com>
2016-08-17 17:41:38 -0500

    got addition functionality working. Realized that I didn't actually put a divide button on. WHOOPS.

--- calc.js
+++ calc.js
@@ -1,23 +1,64 @@
 var screenText  = document.getElementById('screenText');
 var calcButtons = document.getElementsByClassName('calc-btn');
-var operationButtons = document.getElementsByClassName('oper-btn');
+var addButton = document.getElementById('add');
+var subtractButton = document.getElementById('subtract');
+var multiplyButton = document.getElementById('multiply');
+var divideButton = document.getElementById('divide');
+
+var operationEnum = {
+	ADD : 0,
+	SUBTRACT : 1,
+	MULTIPLY : 2,
+	DIVIDE : 3
+};
 
 var calculator = {
 	previousValue : 0,
 	operation : null,
-	operationQueued: false
+	operationQueued: false, 
+	operationTouchedLast: false
+};
+
+// functions for handling button presses
+
+var add = function(){
+	if(calculator.operationQueued == true){
+		var lastNumber = parseFloat(calculator.previousValue);
+		var current = parseFloat(screenText.innerHTML);
+		screenText.innerHTML = current + lastNumber;
+		calculator.previousValue = screenText.innerHTML;
+	}
+	if(calculator.operation == null){
+		calculator.operation = operationEnum.ADD;
+		calculator.previousValue = screenText.innerHTML;
+		calculator.operationQueued = true;
+		screenText.innerHTML = 0;
+	}
+	calculator.operationTouchedLast = true;
 };
 
 var update = function(){
-	if(screenText.innerHTML != 0){
+	if(calculator.operationTouchedLast === true){
+		screenText.innerHTML = this.innerHTML;
+		calculator.operationTouchedLast = false;
+	}
+	else if(screenText.innerHTML != 0){
 		screenText.innerHTML += this.innerHTML;
 	} else {
 		screenText.innerHTML = this.innerHTML;
 	}
+
 };
+
+
+// add event listeners 
 
 for(var i = 0; i < calcButtons.length; i++){
 	calcButtons[i].addEventListener("click",update,false);
 }
 
+addButton.addEventListener("click",add,false);
+subtractButton.addEventListener("click",subtract,false);
+multiplyButton.addEventListener("click",multiply,false);
+//divideButton.addEventListener("click",divide,false);
 
